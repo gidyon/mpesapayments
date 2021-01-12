38d2831d1ecabc8ada6fa7fc5f0756b98d99a559
gidyon <gideon.ngk@gmail.com>
2021-01-12 07:01:14 +0300

    publish base on request param

--- internal/stk/insert.go
+++ internal/stk/insert.go
@@ -25,94 +25,97 @@ func (stkAPI *stkAPIServer) insertWorker(ctx context.Context) {
 	ticker := time.NewTicker(stkAPI.insertTimeOut)
 	defer ticker.Stop()
 
-	stkPayloads := make([]*PayloadStk, 0, bulkInsertSize)
+	incomingPayments := make([]*incomingPayment, 0, bulkInsertSize)
+
+	createFn := func() []*PayloadStk {
+		txs := make([]*PayloadStk, 0, len(incomingPayments))
+		for _, incomingPayment := range incomingPayments {
+			txs = append(txs, incomingPayment.payment)
+		}
+		return txs
+	}
 
 	callback := func(publish bool) {
 
-		if !stkAPI.DisablePublishing && publish {
+		for _, v := range incomingPayments {
 
-			// Publish to consumers
-			stkAPI.Logger.Infof("publishing %d stk result to consumers", len(stkPayloads))
+			// By value because the slice will be reset
+			go func(incomingPayment incomingPayment) {
+				stkPayload := incomingPayment.payment
 
-			for _, stkPayload := range stkPayloads {
+				// Get payload key
+				key := GetMpesaSTKPushKey(stkPayload.PhoneNumber, stkAPI.RedisKeyPrefix)
 
-				// By value because the slice will be reset
-				go func(stkPayload PayloadStk) {
-					// Get payload key
-					key := GetMpesaSTKPushKey(stkPayload.PhoneNumber, stkAPI.RedisKeyPrefix)
+				// Delete key to allow other STK transactions to proceed
+				defer func() {
+					stkAPI.RedisDB.Del(ctx, key)
+				}()
 
-					// Delete key to allow other STK transactions to proceed
-					defer func() {
-						stkAPI.RedisDB.Del(ctx, key)
-					}()
+				publish := false
 
-					publish := false
+				// Get STK initiator id
+				initiatorID, err := stkAPI.RedisDB.Get(ctx, key).Result()
+				switch {
+				case err == nil:
+					publish = true
+				case errors.Is(err, redis.Nil):
+					return
+				default:
+					stkAPI.Logger.Errorf("failed to get initiator key from cache: %v", err)
+					return
+				}
 
-					// Get STK initiator id
-					initiatorID, err := stkAPI.RedisDB.Get(ctx, key).Result()
-					switch {
-					case err == nil:
-						publish = true
-					case errors.Is(err, redis.Nil):
-						stkAPI.Logger.Warningln("no value set in key (initiator key)")
+				// Initiator payload key
+				initiatorKey := GetMpesaSTKPayloadKey(initiatorID, stkAPI.RedisKeyPrefix)
+
+				// Get initiator payload
+				val, err := stkAPI.RedisDB.Get(ctx, initiatorKey).Result()
+				switch {
+				case err == nil:
+					publish = true
+				case errors.Is(err, redis.Nil):
+					stkAPI.Logger.Warningln("no value set in key (initiator payload)")
+					publish = false
+				default:
+					stkAPI.Logger.Errorf("failed to get initiator payload from cache: %v", err)
+					publish = false
+				}
+
+				if publish && stkPayload.Succeeded && incomingPayment.publish {
+					payloadID := valFunc(fmt.Sprint(stkPayload.PayloadID), stkPayload.TransactionID)
+					stkAPI.Logger.Infof("publishing stk %s to consumers", payloadID)
+
+					// Get stk push initiator payload
+					payload := &stk.InitiateSTKPushRequest{}
+					err = proto.Unmarshal([]byte(val), payload)
+					if err != nil {
+						stkAPI.Logger.Errorf("failed unmarshal stk push payload: %v", err)
 						return
-					default:
-						stkAPI.Logger.Errorf("failed to get initiator key from cache: %v", err)
+					}
+
+					// Publish the stk payment to consumers
+					_, err = stkAPI.PublishStkPayload(
+						stkAPI.ctxAdmin, &stk.PublishStkPayloadRequest{
+							PayloadId: payloadID,
+							Payload:   payload.Payload,
+						})
+					if err != nil {
+						stkAPI.Logger.Errorf("failed to publish stk: %v", err)
 						return
 					}
-
-					// Initiator payload key
-					initiatorKey := GetMpesaSTKPayloadKey(initiatorID, stkAPI.RedisKeyPrefix)
-
-					// Get initiator payload
-					val, err := stkAPI.RedisDB.Get(ctx, initiatorKey).Result()
-					switch {
-					case err == nil:
-						publish = true
-					case errors.Is(err, redis.Nil):
-						stkAPI.Logger.Warningln("no value set in key (initiator payload)")
-						publish = false
-					default:
-						stkAPI.Logger.Errorf("failed to get initiator payload from cache: %v", err)
-						publish = false
-					}
-
-					if publish && stkPayload.Succeeded {
-						payloadID := valFunc(fmt.Sprint(stkPayload.PayloadID), stkPayload.MpesaReceiptNumber)
-						stkAPI.Logger.Infof("publishing stk %s to consumers", payloadID)
-
-						// Get stk push initiator payload
-						payload := &stk.InitiateSTKPushRequest{}
-						err = proto.Unmarshal([]byte(val), payload)
-						if err != nil {
-							stkAPI.Logger.Errorf("failed unmarshal stk push payload: %v", err)
-							return
-						}
-
-						// Publish the stk payment to consumers
-						_, err = stkAPI.PublishStkPayload(
-							stkAPI.ctxAdmin, &stk.PublishStkPayloadRequest{
-								PayloadId: payloadID,
-								Payload:   payload.Payload,
-							})
-						if err != nil {
-							stkAPI.Logger.Errorf("failed to publish stk: %v", err)
-							return
-						}
-					}
-				}(*stkPayload)
-			}
+				}
+			}(*v)
 		}
 
 		// Reset stuffs
 		ticker.Reset(stkAPI.insertTimeOut)
-		stkPayloads = stkPayloads[0:0]
+		incomingPayments = incomingPayments[0:0]
 	}
 
 	chanSize := cap(stkAPI.insertChan)
 
 	if chanSize <= 2 {
-		stkAPI.insertChan = make(chan *PayloadStk, 100)
+		stkAPI.insertChan = make(chan *incomingPayment, 100)
 		chanSize = cap(stkAPI.insertChan)
 	}
 
@@ -121,11 +124,11 @@ func (stkAPI *stkAPIServer) insertWorker(ctx context.Context) {
 		case <-ctx.Done():
 			return
 		case <-ticker.C:
-			if len(stkPayloads) > 0 {
-				err := stkAPI.SQLDB.CreateInBatches(stkPayloads, bulkInsertSize).Error
+			if len(incomingPayments) > 0 {
+				err := stkAPI.SQLDB.CreateInBatches(createFn(), bulkInsertSize).Error
 				switch {
 				case err == nil:
-					stkAPI.Logger.Infof("bulk inserted %d stk payloads (from ticker)", len(stkPayloads))
+					stkAPI.Logger.Infof("bulk inserted %d stk payloads (from ticker)", len(incomingPayments))
 					callback(true)
 				case strings.Contains(strings.ToLower(err.Error()), "duplicate"):
 					stkAPI.Logger.Infoln("insert of duplicate stk payloads skipped (from ticker)")
@@ -136,12 +139,12 @@ func (stkAPI *stkAPIServer) insertWorker(ctx context.Context) {
 			}
 
 		case stkPayload := <-stkAPI.insertChan:
-			stkPayloads = append(stkPayloads, stkPayload)
-			if len(stkPayloads) >= (chanSize - 1) {
-				err := stkAPI.SQLDB.CreateInBatches(stkPayloads, bulkInsertSize).Error
+			incomingPayments = append(incomingPayments, stkPayload)
+			if len(incomingPayments) >= (chanSize - 1) {
+				err := stkAPI.SQLDB.CreateInBatches(createFn(), bulkInsertSize).Error
 				switch {
 				case err == nil:
-					stkAPI.Logger.Infof("bulk inserted %d stk payloads (from channel)", len(stkPayloads))
+					stkAPI.Logger.Infof("bulk inserted %d stk payloads (from channel)", len(incomingPayments))
 					callback(true)
 				case strings.Contains(strings.ToLower(err.Error()), "duplicate"):
 					stkAPI.Logger.Infoln("insert of duplicate stk payloads skipped (from channel)")
