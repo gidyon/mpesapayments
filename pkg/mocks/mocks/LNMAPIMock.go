af9d46bdbe8a66709afd1bc7130c916c2bd2419d
gidyon <gideon.ngk@gmail.com>
2021-01-28 16:52:51 +0300

    updated mock field

--- pkg/mocks/mocks/LNMAPIMock.go
+++ pkg/mocks/mocks/LNMAPIMock.go
@@ -18,8 +18,8 @@ type LNMAPIMock struct {
 	mock.Mock
 }
 
-// AddScopes provides a mock function with given fields: ctx, in, opts
-func (_m *LNMAPIMock) AddScopes(ctx context.Context, in *mpesapayment.AddScopesRequest, opts ...grpc.CallOption) (*emptypb.Empty, error) {
+// ArchiveTransactions provides a mock function with given fields: ctx, in, opts
+func (_m *LNMAPIMock) ArchiveTransactions(ctx context.Context, in *mpesapayment.ArchiveTransactionsRequest, opts ...grpc.CallOption) (*emptypb.Empty, error) {
 	_va := make([]interface{}, len(opts))
 	for _i := range opts {
 		_va[_i] = opts[_i]
@@ -30,7 +30,7 @@ func (_m *LNMAPIMock) AddScopes(ctx context.Context, in *mpesapayment.AddScopesR
 	ret := _m.Called(_ca...)
 
 	var r0 *emptypb.Empty
-	if rf, ok := ret.Get(0).(func(context.Context, *mpesapayment.AddScopesRequest, ...grpc.CallOption) *emptypb.Empty); ok {
+	if rf, ok := ret.Get(0).(func(context.Context, *mpesapayment.ArchiveTransactionsRequest, ...grpc.CallOption) *emptypb.Empty); ok {
 		r0 = rf(ctx, in, opts...)
 	} else {
 		if ret.Get(0) != nil {
@@ -39,7 +39,7 @@ func (_m *LNMAPIMock) AddScopes(ctx context.Context, in *mpesapayment.AddScopesR
 	}
 
 	var r1 error
-	if rf, ok := ret.Get(1).(func(context.Context, *mpesapayment.AddScopesRequest, ...grpc.CallOption) error); ok {
+	if rf, ok := ret.Get(1).(func(context.Context, *mpesapayment.ArchiveTransactionsRequest, ...grpc.CallOption) error); ok {
 		r1 = rf(ctx, in, opts...)
 	} else {
 		r1 = ret.Error(1)
@@ -310,6 +310,36 @@ func (_m *LNMAPIMock) PublishMpesaPayment(ctx context.Context, in *mpesapayment.
 
 	var r1 error
 	if rf, ok := ret.Get(1).(func(context.Context, *mpesapayment.PublishMpesaPaymentRequest, ...grpc.CallOption) error); ok {
+		r1 = rf(ctx, in, opts...)
+	} else {
+		r1 = ret.Error(1)
+	}
+
+	return r0, r1
+}
+
+// SaveScopes provides a mock function with given fields: ctx, in, opts
+func (_m *LNMAPIMock) SaveScopes(ctx context.Context, in *mpesapayment.SaveScopesRequest, opts ...grpc.CallOption) (*emptypb.Empty, error) {
+	_va := make([]interface{}, len(opts))
+	for _i := range opts {
+		_va[_i] = opts[_i]
+	}
+	var _ca []interface{}
+	_ca = append(_ca, ctx, in)
+	_ca = append(_ca, _va...)
+	ret := _m.Called(_ca...)
+
+	var r0 *emptypb.Empty
+	if rf, ok := ret.Get(0).(func(context.Context, *mpesapayment.SaveScopesRequest, ...grpc.CallOption) *emptypb.Empty); ok {
+		r0 = rf(ctx, in, opts...)
+	} else {
+		if ret.Get(0) != nil {
+			r0 = ret.Get(0).(*emptypb.Empty)
+		}
+	}
+
+	var r1 error
+	if rf, ok := ret.Get(1).(func(context.Context, *mpesapayment.SaveScopesRequest, ...grpc.CallOption) error); ok {
 		r1 = rf(ctx, in, opts...)
 	} else {
 		r1 = ret.Error(1)
