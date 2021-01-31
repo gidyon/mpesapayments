a66585218517f19b8c9f71bea07a3510769cbfda
gidyon <gideon.ngk@gmail.com>
2021-01-31 12:24:37 +0300

    bug fix when checking if response from mpesa was successful

--- pkg/payload/response.go
+++ pkg/payload/response.go
@@ -19,54 +19,57 @@ func (apiRes *APIResponse) Succeeded() bool {
 }
 
 // GenericAPIResponse is generic API response
-type GenericAPIResponse map[string]interface{}
+type GenericAPIResponse struct {
+	Response map[string]string
+}
 
 // Succeeded checks whether the request succeeded
 func (gRes *GenericAPIResponse) Succeeded() bool {
-	if gRes.ErrorCode() != "" || gRes.ErrorMessage() != "" {
-		return false
-	}
 	if gRes.ResponseCode() == "0" || strings.Contains(gRes.ResponseDescription(), "successfully") {
 		return true
+	}
+	if (gRes.ErrorCode() != "" && strings.Contains(gRes.ErrorMessage(), "nil") == false) ||
+		(gRes.ErrorMessage() != "" && strings.Contains(gRes.ErrorMessage(), "nil") == false) {
+		return false
 	}
 	return false
 }
 
 // ResponseCode returns the response code
-func (gRes GenericAPIResponse) ResponseCode() string {
-	return fmt.Sprint(map[string]interface{}(gRes)["ResponseCode"])
+func (gRes *GenericAPIResponse) ResponseCode() string {
+	return fmt.Sprint(gRes.Response["ResponseCode"])
 }
 
 // ResponseDescription returns the response description
-func (gRes GenericAPIResponse) ResponseDescription() string {
-	return fmt.Sprint(map[string]interface{}(gRes)["ResponseDescription"])
+func (gRes *GenericAPIResponse) ResponseDescription() string {
+	return fmt.Sprint(gRes.Response["ResponseDescription"])
 }
 
 // OriginatorConversationID returns the originator conversation id
-func (gRes GenericAPIResponse) OriginatorConversationID() string {
-	return fmt.Sprint(map[string]interface{}(gRes)["OriginatorConversationId"])
+func (gRes *GenericAPIResponse) OriginatorConversationID() string {
+	return fmt.Sprint(gRes.Response["OriginatorConversationID"])
 }
 
 // ConversationID returns the conversation id
-func (gRes GenericAPIResponse) ConversationID() string {
-	return fmt.Sprint(map[string]interface{}(gRes)["ConversationId"])
+func (gRes *GenericAPIResponse) ConversationID() string {
+	return fmt.Sprint(gRes.Response["ConversationID"])
 }
 
 // ErrorCode returns the error code
-func (gRes GenericAPIResponse) ErrorCode() string {
-	return fmt.Sprint(map[string]interface{}(gRes)["errorCode"])
+func (gRes *GenericAPIResponse) ErrorCode() string {
+	return fmt.Sprint(gRes.Response["errorCode"])
 }
 
 // ErrorMessage returns the error message
-func (gRes GenericAPIResponse) ErrorMessage() string {
-	return fmt.Sprint(map[string]interface{}(gRes)["errorMessage"])
+func (gRes *GenericAPIResponse) ErrorMessage() string {
+	return fmt.Sprint(gRes.Response["errorMessage"])
 }
 
 // Error returns the error
-func (gRes GenericAPIResponse) Error() string {
-	if gRes.ErrorMessage() != "" {
+func (gRes *GenericAPIResponse) Error() string {
+	if gRes.ErrorMessage() != "" && strings.Contains(gRes.ErrorMessage(), "nil") == false {
 		return gRes.ErrorMessage()
-	} else if gRes.ResponseDescription() != "" {
+	} else if gRes.ResponseDescription() != "" && strings.Contains(gRes.ResponseDescription(), "nil") {
 		return gRes.ResponseDescription()
 	} else {
 		return "response has no message or description"
