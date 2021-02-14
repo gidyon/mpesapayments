4132f5cfbd47a651f5e14a01e5232a743a0334df
gidyon <gideon.ngk@gmail.com>
2021-02-14 19:06:23 +0300

    added handler to receive onfon transactions

--- cmd/mpesapayment/transaction.go
+++ cmd/mpesapayment/transaction.go
@@ -2,16 +2,20 @@ package main
 
 import (
 	"context"
-	"encoding/json"
 	"fmt"
 	"net/http"
 	"time"
 
+	"encoding/json"
+	"errors"
+
 	"github.com/gidyon/micro/v2/pkg/middleware/grpc/auth"
 	b2capp "github.com/gidyon/mpesapayments/internal/b2c"
 	"github.com/gidyon/mpesapayments/pkg/api/b2c"
-	"github.com/gidyon/mpesapayments/pkg/payload"
 	"google.golang.org/grpc/metadata"
+
+	"github.com/gidyon/mpesapayments/pkg/payload"
+	"github.com/go-redis/redis/v8"
 	"google.golang.org/protobuf/proto"
 )
 
@@ -64,22 +68,18 @@ func (gw *b2cGateway) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	}
 
 	var (
-		initiatorID     = r.URL.Query().Get(b2capp.InitiatorID)
-		requestID       = r.URL.Query().Get(b2capp.RequestIDQuery)
-		shortCode       = r.URL.Query().Get(b2capp.ShortCodeQuery)
-		msisdn          = r.URL.Query().Get(b2capp.MSISDNQuery)
-		txType          = r.URL.Query().Get(b2capp.TxTypeQuery)
-		publishLocal    = r.URL.Query().Get(b2capp.PublishLocalQuery) != ""
-		publishOnCreate = r.URL.Query().Get(b2capp.PublishGlobalQuery) != ""
-		drop            = r.URL.Query().Get(b2capp.DropQuery) != ""
-		ctx             = r.Context()
-		err             error
+		initiatorID = r.URL.Query().Get(b2capp.InitiatorID)
+		requestID   = r.URL.Query().Get(b2capp.RequestIDQuery)
 	)
 
-	// Tell local consumers that they can unsubscribe
-	defer func() {
-		gw.RedisDB.Publish(context.Background(), b2capp.AddPrefix(gw.Options.B2CLocalTopic, gw.Options.RedisKeyPrefix), requestID)
-	}()
+	if initiatorID == "" && requestID == "" {
+		gw.fromOnfon(w, r)
+	} else {
+		gw.fromSaf(w, r)
+	}
+}
+
+func (gw *b2cGateway) fromSaf(w http.ResponseWriter, r *http.Request) {
 
 	gw.Logger.Infoln("received b2c transaction from mpesa")
 
@@ -90,7 +90,10 @@ func (gw *b2cGateway) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	transaction := &payload.Transaction{}
+	var (
+		transaction = &payload.Transaction{}
+		err         error
+	)
 
 	switch r.Header.Get("content-type") {
 	case "application/json", "application/json;charset=UTF-8":
@@ -125,13 +128,65 @@ func (gw *b2cGateway) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	var (
+		initiatorID     = r.URL.Query().Get(b2capp.InitiatorID)
+		requestID       = r.URL.Query().Get(b2capp.RequestIDQuery)
+		shortCode       = r.URL.Query().Get(b2capp.ShortCodeQuery)
+		msisdn          = r.URL.Query().Get(b2capp.MSISDNQuery)
+		txType          = r.URL.Query().Get(b2capp.TxTypeQuery)
+		publishLocal    = r.URL.Query().Get(b2capp.PublishLocalQuery) != ""
+		publishOnCreate = r.URL.Query().Get(b2capp.PublishGlobalQuery) != ""
+		drop            = r.URL.Query().Get(b2capp.DropQuery) != ""
+		ctx             = r.Context()
+	)
+
+	initiator := &b2c.InitiatorPayload{
+		InitiatorId:     initiatorID,
+		RequestId:       requestID,
+		ShortCode:       shortCode,
+		Msisdn:          msisdn,
+		TransactionType: txType,
+		Source:          "MPESA",
+		PublishLocal:    publishLocal,
+		PublishOnCreate: publishOnCreate,
+		DropTransaction: drop,
+	}
+	if initiatorID == "" {
+		// Get initiator
+		key := b2capp.GetInitiatorKey(transaction.MSISDN())
+
+		res, err := gw.RedisDB.Get(ctx, key).Result()
+		switch {
+		case err == nil:
+			err = proto.Unmarshal([]byte(res), initiator)
+			if err != nil {
+				gw.Logger.Errorf("failed to unmarshal initiator: %v", err)
+				http.Error(w, err.Error(), http.StatusBadRequest)
+				return
+			}
+		case errors.Is(err, redis.Nil):
+		default:
+			gw.Logger.Errorf("failed to get initiator from cache: %v", err)
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+	}
+
+	if initiator.PublishLocal {
+		// Tell local consumers that they can unsubscribe
+		defer func() {
+			gw.RedisDB.Publish(ctx, b2capp.AddPrefix(gw.Options.B2CLocalTopic, gw.Options.RedisKeyPrefix), initiator.RequestId)
+		}()
+	}
+
 	// Transaction payload
 	transactionPB := &b2c.B2CPayment{
-		InitiatorId:              initiatorID,
-		OrgShortCode:             shortCode,
-		Msisdn:                   msisdn,
+		InitiatorId:              initiator.InitiatorId,
+		OrgShortCode:             initiator.ShortCode,
+		Msisdn:                   transaction.MSISDN(),
 		ReceiverPartyPublicName:  transaction.ReceiverPartyPublicName(),
-		TransactionType:          txType,
+		TransactionType:          initiator.TransactionType,
+		TransactionId:            transaction.TransactionReceipt(),
 		ConversationId:           transaction.Result.ConversationID,
 		OriginatorConversationId: transaction.Result.OriginatorConversationID,
 		ResultCode:               fmt.Sprint(transaction.Result.ResultCode),
@@ -145,7 +200,7 @@ func (gw *b2cGateway) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		Succeeded:                transaction.Succeeded(),
 	}
 
-	if publishLocal && requestID != "" {
+	if initiator.PublishLocal && initiator.RequestId != "" {
 		// Marshal
 		bs, err := proto.Marshal(transactionPB)
 		if err != nil {
@@ -155,7 +210,7 @@ func (gw *b2cGateway) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		}
 
 		// Set transaction in cache
-		err = gw.RedisDB.Set(ctx, addPrefix(requestID, gw.RedisKeyPrefix), bs, 5*time.Minute).Err()
+		err = gw.RedisDB.Set(ctx, addPrefix(initiator.RequestId, gw.RedisKeyPrefix), bs, 5*time.Minute).Err()
 		if err != nil {
 			http.Error(w, "failed to set b2c transaction in cache", http.StatusInternalServerError)
 			gw.Logger.Errorln("failed to set b2c transaction in cache: %s", err)
@@ -163,7 +218,7 @@ func (gw *b2cGateway) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		}
 
 		// Publish the transaction for local goroutines
-		err = gw.RedisDB.Publish(ctx, gw.B2CLocalTopic, requestID).Err()
+		err = gw.RedisDB.Publish(ctx, gw.B2CLocalTopic, initiator.RequestId).Err()
 		if err != nil {
 			http.Error(w, "failed to publish b2c to local listener", http.StatusInternalServerError)
 			gw.Logger.Errorln("failed to publish b2c transaction to local listener: %s", err)
@@ -171,24 +226,168 @@ func (gw *b2cGateway) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		}
 	}
 
-	if !drop {
+	if initiator.DropTransaction == false {
 		// We save the transaction
 		_, err = gw.b2cAPI.CreateB2CPayment(gw.ctxExt, &b2c.CreateB2CPaymentRequest{
 			Payment: transactionPB,
-			Publish: publishOnCreate,
+			Publish: initiator.PublishOnCreate,
 		})
 		if err != nil {
 			gw.Logger.Errorf("failed to create b2c payment: %v", err)
 			http.Error(w, err.Error(), http.StatusInternalServerError)
-		}
 
-		// Save in cache for later processing
-		bs, err := proto.Marshal(transactionPB)
-		if err == nil {
-			gw.RedisDB.LPush(r.Context(), b2capp.FailedTxList, bs)
+			// Save in cache for later processing
+			bs, err := proto.Marshal(transactionPB)
+			if err == nil {
+				gw.RedisDB.LPush(r.Context(), b2capp.FailedTxList, bs)
+			}
+			return
 		}
+	}
 
+	w.Write([]byte("mpesa b2c transaction processed"))
+}
+
+func (gw *b2cGateway) fromOnfon(w http.ResponseWriter, r *http.Request) {
+
+	gw.Logger.Infoln("received b2c transaction from onfon")
+
+	// Must be POST request
+	if r.Method != http.MethodPost {
+		gw.Logger.Infof("only post allowed; hot %v http method", r.Method)
+		http.Error(w, "bad method; only POST allowed", http.StatusBadRequest)
 		return
+	}
+
+	var (
+		transaction = &payload.IncomingTransactionOnfon{}
+		ctx         = r.Context()
+		err         error
+	)
+
+	switch r.Header.Get("content-type") {
+	case "application/json", "application/json;charset=UTF-8":
+		// Marshaling
+		err = json.NewDecoder(r.Body).Decode(transaction)
+		if err != nil {
+			gw.Logger.Errorf("error decoding json in response: %v", err)
+			http.Error(w, "decoding json failed: "+err.Error(), http.StatusBadRequest)
+			return
+		}
+	default:
+		ctype := r.Header.Get("content-type")
+		http.Error(w, fmt.Sprintf("incorrect content-type: %s", ctype), http.StatusBadRequest)
+		gw.Logger.Warningln("incorrect content type: %s", ctype)
+		return
+	}
+
+	// Validation
+	switch {
+	case transaction == nil:
+		err = fmt.Errorf("nil mpesa transaction")
+	case transaction.ConversationID == "":
+		err = fmt.Errorf("missing conversation id")
+	case transaction.OriginatorConversationID == "":
+		err = fmt.Errorf("missing originator id")
+	case transaction.ResultDesc == "":
+		err = fmt.Errorf("missing description")
+	}
+	if err != nil {
+		gw.Logger.Errorf("validation error: %v", err)
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	initiator := &b2c.InitiatorPayload{}
+
+	// Get initiator
+	key := b2capp.GetInitiatorKey(transaction.MSISDN())
+
+	res, err := gw.RedisDB.Get(ctx, key).Result()
+	switch {
+	case err == nil:
+		err = proto.Unmarshal([]byte(res), initiator)
+		if err != nil {
+			gw.Logger.Errorf("failed to unmarshal initiator: %v", err)
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+	case errors.Is(err, redis.Nil):
+	default:
+		gw.Logger.Errorf("failed to get initiator from cache: %v", err)
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	if initiator.PublishLocal {
+		// Tell local consumers that they can unsubscribe
+		defer func() {
+			gw.RedisDB.Publish(ctx, b2capp.AddPrefix(gw.Options.B2CLocalTopic, gw.Options.RedisKeyPrefix), initiator.RequestId)
+		}()
+	}
+
+	// Transaction payload
+	transactionPB := &b2c.B2CPayment{
+		InitiatorId:              initiator.InitiatorId,
+		OrgShortCode:             initiator.ShortCode,
+		Msisdn:                   transaction.MSISDN(),
+		ReceiverPartyPublicName:  transaction.ReceiverPartyPublicName,
+		TransactionType:          initiator.TransactionType,
+		TransactionId:            transaction.TransactionID,
+		ConversationId:           transaction.ConversationID,
+		OriginatorConversationId: transaction.OriginatorConversationID,
+		ResultCode:               fmt.Sprint(transaction.ResultCode),
+		ResultDescription:        transaction.ResultDesc,
+		TransactionTimestamp:     transaction.CompletedDateTime().Unix(),
+		Amount:                   float32(transaction.Amount()),
+		WorkingAccountFunds:      float32(transaction.B2CUtilityAccountAvailableFundsV2()),
+		RecipientRegistered:      transaction.B2CRecipientIsRegisteredCustomerV2(),
+		Succeeded:                transaction.Succeeded(),
+	}
+
+	if initiator.PublishLocal && initiator.RequestId != "" {
+		// Marshal
+		bs, err := proto.Marshal(transactionPB)
+		if err != nil {
+			http.Error(w, "marshaling b2c payload not succeesful", http.StatusInternalServerError)
+			gw.Logger.Errorln("marshaling b2c payload not succeesful: %s", err)
+			return
+		}
+
+		// Set transaction in cache
+		err = gw.RedisDB.Set(ctx, addPrefix(initiator.RequestId, gw.RedisKeyPrefix), bs, 5*time.Minute).Err()
+		if err != nil {
+			http.Error(w, "failed to set b2c transaction in cache", http.StatusInternalServerError)
+			gw.Logger.Errorln("failed to set b2c transaction in cache: %s", err)
+			return
+		}
+
+		// Publish the transaction for local goroutines
+		err = gw.RedisDB.Publish(ctx, gw.B2CLocalTopic, initiator.RequestId).Err()
+		if err != nil {
+			http.Error(w, "failed to publish b2c to local listener", http.StatusInternalServerError)
+			gw.Logger.Errorln("failed to publish b2c transaction to local listener: %s", err)
+			return
+		}
+	}
+
+	if initiator.DropTransaction == false {
+		// We save the transaction
+		_, err = gw.b2cAPI.CreateB2CPayment(gw.ctxExt, &b2c.CreateB2CPaymentRequest{
+			Payment: transactionPB,
+			Publish: initiator.PublishOnCreate,
+		})
+		if err != nil {
+			gw.Logger.Errorf("failed to create b2c payment: %v", err)
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+
+			// Save in cache for later processing
+			bs, err := proto.Marshal(transactionPB)
+			if err == nil {
+				gw.RedisDB.LPush(r.Context(), b2capp.FailedTxList, bs)
+			}
+			return
+		}
 	}
 
 	w.Write([]byte("mpesa b2c transaction processed"))
