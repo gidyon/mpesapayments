90e6d10e11116fa914045807dcb7b449c4370350
gidyon <gideon.ngk@gmail.com>
2021-03-01 16:35:28 +0300

    test suite for list stats

---
+++ internal/b2c/list_stat_test.go
@@ -0,0 +1,75 @@
+package b2c
+
+import (
+	"context"
+	"time"
+
+	"github.com/gidyon/mpesapayments/pkg/api/b2c"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+)
+
+var _ = Describe("Listing transaction stats @liststat", func() {
+	var (
+		ctx     context.Context
+		listReq *b2c.ListDailyStatsRequest
+	)
+
+	BeforeEach(func() {
+		ctx = context.Background()
+		listReq = &b2c.ListDailyStatsRequest{
+			PageToken: "",
+			PageSize:  20,
+			Filter:    &b2c.ListStatsFilter{},
+		}
+	})
+
+	Describe("Listing for stats for mpesa transactions with malformed request", func() {
+		It("should fail when the request", func() {
+			listReq = nil
+			listRes, err := B2CAPI.ListDailyStats(ctx, *&listReq)
+			Expect(err).Should(HaveOccurred())
+			Expect(status.Code(err)).Should(Equal(codes.InvalidArgument))
+			Expect(listRes).Should(BeNil())
+		})
+	})
+
+	Describe("Getting stats with well-formed request", func() {
+		Context("We create some random transactions", func() {
+			It("should always succeed", func() {
+				var err error
+				payments := make([]*Payment, 0, 50)
+				for i := 0; i < 100; i++ {
+					paymentDB, err := GetB2CPaymentDB(fakeB2CPayment())
+					Expect(err).ShouldNot(HaveOccurred())
+
+					payments = append(payments, paymentDB)
+				}
+				err = B2CAPIServer.SQLDB.CreateInBatches(payments, 50).Error
+				Expect(err).ShouldNot(HaveOccurred())
+
+				// Lets wait for stats to be generated
+				B2CAPIServer.Logger.Infoln("waiting 5sec for worker to calculate statistics")
+				time.Sleep(5 * time.Second)
+			})
+		})
+
+		Describe("Listing stats with well-formed request", func() {
+			var (
+				pageToken     string
+				nextPageToken = "q"
+			)
+			It("should succeed", func() {
+				for nextPageToken != "" {
+					listReq.PageToken = pageToken
+					listRes, err := B2CAPI.ListDailyStats(ctx, listReq)
+					Expect(err).ShouldNot(HaveOccurred())
+					Expect(status.Code(err)).Should(Equal(codes.OK))
+					Expect(listRes).ShouldNot(BeNil())
+					nextPageToken = listRes.NextPageToken
+					pageToken = listRes.NextPageToken
+				}
+			})
+		})
+	})
+})
