8279081937367bf92350034fdcc08c58fba7d5cb
gidyon <gideon.ngk@gmail.com>
2022-07-20 15:37:18 +0300

    added mpesa incoming payload for till

--- pkg/payload/request.go
+++ pkg/payload/request.go
@@ -16,3 +16,20 @@ type MpesaPayload struct {
 	MiddleName        string `json:"middle_name,omitempty"`
 	LastName          string `json:"last_name,omitempty"`
 }
+
+// MpesaPayloadV2 is incoming paybill transaction from mpesa
+type MpesaPayloadV2 struct {
+	TransactionType   string `json:"TransactionType"`
+	TransID           string `json:"TransID"`
+	TransTime         string `json:"TransTime"`
+	TransAmount       string `json:"TransAmount"`
+	BusinessShortCode string `json:"BusinessShortCode"`
+	BillRefNumber     string `json:"BillRefNumber"`
+	InvoiceNumber     string `json:"InvoiceNumber"`
+	OrgAccountBalance string `json:"OrgAccountBalance"`
+	ThirdPartyTransID string `json:"ThirdPartyTransID"`
+	MSISDN            string `json:"MSISDN"`
+	FirstName         string `json:"FirstName"`
+	MiddleName        string `json:"MiddleName"`
+	LastName          string `json:"LastName"`
+}
